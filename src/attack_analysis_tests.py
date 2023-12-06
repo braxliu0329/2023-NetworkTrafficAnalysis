59da8fab2e73e5f77be1d95e25781695d952d9ee
TomosSherlock <he22266@bristol.ac.uk>
2023-12-06 16:00:28 +0000

    Changed a comment so that the CI tests should run.

--- src/attack_analysis_tests.py
+++ src/attack_analysis_tests.py
@@ -52,7 +52,7 @@ class MyTestCase(unittest.TestCase):
 
         # ensure that the method does not incur any false positives
         self.assertFalse(false_suspicious_addresses, "The TCP Flood Attack Analysis has detected false suspicious "
-                                                     "addresses")
+                                                     "addresses ")
         self.assertFalse(false_attacked_addresses, "The TCP Flood Attack Analysis has detected false victim "
                                                    "addresses")
 
