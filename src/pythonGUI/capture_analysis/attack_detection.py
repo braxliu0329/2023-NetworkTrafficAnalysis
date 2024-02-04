ef115c2d3113967535af81840edba2e767fab568
T0NYLiu <146854378+T0NYLiu@users.noreply.github.com>
2024-02-04 00:10:37 +0000

    Update attack_detection.py
    Update from last years project with new UDP flood detection in attack_detection.py.

--- src/pythonGUI/capture_analysis/attack_detection.py
+++ src/pythonGUI/capture_analysis/attack_detection.py
@@ -500,6 +500,37 @@ class AttackDetection:
         # Returns both graphs
         return [canvas, canvas2]
 
+        # -------------Below are new attack methods----------------
+    
+    def udp_flood_detect(self, threshold):
+        # initialise udp suspicious addresses
+        self.udp_suspicious = []
+        # create an empty canvas to store data points
+        canvas = EmbeddedCanvas()
+        # dataframe with only UDP packets
+        udp_packets = self.dataframe[self.dataframe['Protocol'] == 'UDP']
+        # if there are no UDP packets, return nothing as there can be no udp flood attacks
+        if udp_packets.empty:
+            return None
+        
+        # removes any duplicated addresses
+        udp_addresses = udp_packets['SourceIP'].unique()
+        # create the packets per second table using the calc_pps function. This will also populate the suspicious list
+        # with all udp addresses that have a pps above the provided threshold
+        pps_table = self.calc_pps(udp_addresses, udp_packets, self.udp_suspicious, threshold)
+
+        # creates dataframe from dictionary
+        pps_dataframe = pd.DataFrame.from_dict(pps_table)
+
+        # creates graph from dataframe
+        pps_graph = pps_dataframe.plot(ax=canvas.axes, kind='barh', x="Address", legend=False)
+        pps_graph.set(title="UDP Flood Detection", xlabel="Packets Per Second")
+        pps_graph.locator_params(axis="x", integer=True, tight=True)
+        pps_graph.axvline(threshold, color='r', linestyle='--')
+
+        # return the created graph to be represented on the GUI
+        return canvas
+
     def calc_pps(self, suspicious_addresses, packets, attack_sus_list, threshold):
         # dictionary of the addresses and their packets per second
         pps_table = {'Address': [],
