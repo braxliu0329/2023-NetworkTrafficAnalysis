f22d96851cddc084436c38c762b2d2f8930167f2
AniBoja <aniboja26@gmail.com>
2024-02-13 16:51:18 +0000

    Added bar graph visualisation for protocol frequency.

--- src/analysis/client/src/components/FrequencyGraph.tsx
+++ src/analysis/client/src/components/FrequencyGraph.tsx
@@ -1,100 +1,100 @@
 import { ResponsiveBar } from '@nivo/bar'
 
+
+
 interface FrequencyGraphProps {
     data: any[]
 };
 
 const FrequencyGraph: React.FC<FrequencyGraphProps> = ({ data }) => {
+    const getColor = (bar: any) => {
+        const colorMap: { [key: string]: string} = {
+            'TCP': '#003366',
+            'UDP': '#006633',
+            'UDP/DNS': '#006633',
+            'IGMP': '#660066',
+            'ICMP': '#006666',
+            'ARP': '#660000',
+            'IPv6': '#666600'
+        };
+        if (colorMap.hasOwnProperty(bar.indexValue)) {
+            return colorMap[bar.indexValue];
+        }
+        return 'Grey'
+    }
     return (
         <ResponsiveBar
             data={data}
-            keys={['protocol']}
-            indexBy="frequency"
+            keys={['frequency']}
+            indexBy="protocol"
+            layout="horizontal"
             margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
             padding={0.3}
-            groupMode="grouped"
-            layout="horizontal"
-            valueScale={{ type: 'linear' }}
-            indexScale={{ type: 'band', round: true }}
-            colors={{ scheme: 'nivo' }}
-            defs={[
-                {
-                    id: 'dots',
-                    type: 'patternDots',
-                    background: 'inherit',
-                    color: '#38bcb2',
-                    size: 4,
-                    padding: 1,
-                    stagger: true
-                },
-                {
-                    id: 'lines',
-                    type: 'patternLines',
-                    background: 'inherit',
-                    color: '#eed312',
-                    rotation: -45,
-                    lineWidth: 6,
-                    spacing: 10
-                }
-            ]}
+            colors={getColor}
+            borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
             axisTop={null}
             axisRight={null}
             axisBottom={{
+                legend: "Frequency",
+                legendPosition: "middle",
+                legendOffset: 36,
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: 'frequency',
-                legendPosition: 'middle',
-                legendOffset: 32,
-                truncateTickAt: 0
             }}
             axisLeft={{
+                legend: "Protocol",
+                legendPosition: "middle",
+                legendOffset: -50,
                 tickSize: 5,
                 tickPadding: 5,
                 tickRotation: 0,
-                legend: 'protocol',
-                legendPosition: 'middle',
-                legendOffset: -40,
-                truncateTickAt: 0
+                
             }}
             labelSkipWidth={12}
             labelSkipHeight={12}
-            labelTextColor={{
-                from: 'color',
-                modifiers: [
-                    [
-                        'darker',
-                        1.6
-                    ]
-                ]
+            labelTextColor={'white'}
+            theme={{
+                axis:{
+                    ticks: {
+                        text: {
+                            fill: "#c79eba"
+                        }
+                    },
+                    legend: {
+                        text:{
+                            fill: "#ab6d98"
+                        }
+                    }
+                }
             }}
             legends={[
                 {
-                    dataFrom: 'keys',
-                    anchor: 'bottom-right',
-                    direction: 'column',
-                    justify: false,
-                    translateX: 120,
-                    translateY: 0,
-                    itemsSpacing: 2,
-                    itemWidth: 100,
-                    itemHeight: 20,
-                    itemDirection: 'left-to-right',
-                    itemOpacity: 0.85,
-                    symbolSize: 20,
-                    effects: [
-                        {
-                            on: 'hover',
-                            style: {
-                                itemOpacity: 1
-                            }
-                        }
-                    ]
-                }
+                dataFrom: 'indexes',
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 120,
+                translateY: 0,
+                itemsSpacing: 2,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: 'left-to-right',
+                itemOpacity: 0.85,
+                itemTextColor: "white",
+                symbolSize: 20,
+                effects: [
+                    {
+                    on: 'hover',
+                    style: {
+                        itemOpacity: 1,
+                    },
+                    },
+                ],
+                },
             ]}
-            role="application"
-            ariaLabel="Nivo bar chart demo"
-        />
+            animate={true}
+    />
     );
 }
 
