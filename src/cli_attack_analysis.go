e56f947cf60fd6526ceb9652c0a7c52d838df5de
TomosSherlock <he22266@bristol.ac.uk>
2024-02-16 13:32:02 +0000

    Recreated TCPConnectScanningFlood method. 2 tests pass.

--- src/cli_attack_analysis.go
+++ src/cli_attack_analysis.go
@@ -4,6 +4,8 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"sort"
+	"time"
 )
 
 // Report struct to contain slices of suspicious and attacked addresses
@@ -21,11 +23,12 @@ type TCPAddressData struct {
 	ReceivesACK int
 }
 
-// TCPAddress struct to pair tcp packets with their source and Destination
-type TCPAddress struct {
+// TCPPacketAddress struct to pair tcp packets with their source and Destination
+type TCPPacketAddress struct {
 	Source      gopacket.Endpoint
 	Destination gopacket.Endpoint
 	TcpPacket   *layers.TCP
+	Packet      gopacket.Packet
 }
 
 func main() {
@@ -52,9 +55,9 @@ func getPackets(path string) []gopacket.Packet {
 }
 
 // getTCP extracts all TCP packet layers from a slice of packets
-func getTCP(packets []gopacket.Packet) []TCPAddress {
+func getTCP(packets []gopacket.Packet) []TCPPacketAddress {
 	// slice to return tcp packets
-	var tcpPackets []TCPAddress
+	var tcpPackets []TCPPacketAddress
 	// *threaded* loop through all packets
 	for _, packet := range packets {
 		// assign a variable to the TCP layer if it exists. If it doesn't exist, move on.
@@ -65,7 +68,7 @@ func getTCP(packets []gopacket.Packet) []TCPAddress {
 			source := packet.NetworkLayer().NetworkFlow().Src()
 			dest := packet.NetworkLayer().NetworkFlow().Dst()
 			// add the tcp layer and address to the slice
-			tcpPackets = append(tcpPackets, TCPAddress{source, dest, tcp})
+			tcpPackets = append(tcpPackets, TCPPacketAddress{source, dest, tcp, packet})
 		}
 	}
 
@@ -73,26 +76,8 @@ func getTCP(packets []gopacket.Packet) []TCPAddress {
 	return tcpPackets
 }
 
-// maskAddress turns an endpoint into a string address by taking the first and last 3 sections
-func maskAddress(source gopacket.Endpoint) string {
-	//// get the full address chunks as a string
-	//addressBytes := strings.Split(source.String(), ".")
-	//// return the wanted masked address
-	//return addressBytes[0] + addressBytes[4] + addressBytes[5] + addressBytes[6]
-	return source.String()
-}
-
-// tcpSynFloodDetect uses the name of a pcap file and returns lists containing any suspicious and suspected attacked addresses
-func tcpSynFloodDetect(file string) Report {
-	// get packets from a pcap file
-	packets := getPackets(file)
-	// extract all TCP layers
-	tcpPackets := getTCP(packets)
-
-	// slices to contain suspicious and attacked addresses
-	var suspiciousAddresses []string
-	var attackedAddresses []string
-
+// getTCPAddressData uses a slice of TCP packets to collate data for each address to be returned as a slice
+func getTCPAddressData(tcpPackets []TCPPacketAddress) []TCPAddressData {
 	// slice to store tcp address information
 	var addresses []TCPAddressData
 	// *threaded* obtain all tcp address information
@@ -115,7 +100,7 @@ func tcpSynFloodDetect(file string) Report {
 				if packet.TcpPacket.ACK {
 					ack = 1
 				}
-				address = TCPAddressData{packet.Source, address.SendsSYN + syn, address.SendsAck + ack, address.ReceivesSyn, address.ReceivesACK}
+				address = TCPAddressData{packet.Source, address.SendsSYN, address.SendsAck, address.ReceivesSyn + syn, address.ReceivesACK + ack}
 			}
 			if packet.Destination == address.Address {
 				newDestination = false
@@ -130,7 +115,7 @@ func tcpSynFloodDetect(file string) Report {
 				if packet.TcpPacket.ACK {
 					ack = 1
 				}
-				address = TCPAddressData{packet.Destination, address.SendsSYN, address.SendsAck, address.ReceivesSyn + syn, address.ReceivesACK + ack}
+				address = TCPAddressData{packet.Destination, address.SendsSYN + syn, address.SendsAck + ack, address.ReceivesSyn, address.ReceivesACK}
 			}
 		}
 		// if the packet is from a new source, add it to the slice
@@ -146,7 +131,7 @@ func tcpSynFloodDetect(file string) Report {
 			if packet.TcpPacket.ACK {
 				ack = 1
 			}
-			addresses = append(addresses, TCPAddressData{packet.Source, syn, ack, 0, 0})
+			addresses = append(addresses, TCPAddressData{packet.Source, 0, 0, syn, ack})
 		}
 		// if the packet is from a new destination, add it to the slice
 		if newDestination {
@@ -162,19 +147,35 @@ func tcpSynFloodDetect(file string) Report {
 			if packet.TcpPacket.ACK {
 				ack = 1
 			}
-			addresses = append(addresses, TCPAddressData{packet.Destination, 0, 0, syn, ack})
+			addresses = append(addresses, TCPAddressData{packet.Destination, syn, ack, 0, 0})
 		}
 	}
+	// return found address data
+	return addresses
+}
+
+// tcpSynFloodDetect uses the name of a pcap file and returns lists containing any suspicious and suspected attacked addresses
+func tcpSynFloodDetect(file string) Report {
+	// get packets from a pcap file
+	packets := getPackets(file)
+	// extract all TCP layers
+	tcpPackets := getTCP(packets)
+	// collate address data
+	addresses := getTCPAddressData(tcpPackets)
+
+	// slices to contain suspicious and attacked addresses
+	var suspiciousAddresses []string
+	var attackedAddresses []string
 
 	// *threaded* loop through obtained addresses
 	for _, address := range addresses {
-		// mark the address as suspicious if it receives more SYN packets than SYN-ACK packets sent
+		// mark the address as attacked if it receives more SYN packets than SYN-ACK packets sent
 		if address.ReceivesSyn > 3*address.SendsAck/2 {
-			suspiciousAddresses = append(suspiciousAddresses, maskAddress(address.Address))
+			attackedAddresses = append(attackedAddresses, address.Address.String())
 		}
 		// mark the address as suspicious if it sends more SYN packets than SYN-ACK packets received
 		if address.SendsSYN > 3*address.ReceivesACK/2 {
-			attackedAddresses = append(attackedAddresses, maskAddress(address.Address))
+			suspiciousAddresses = append(suspiciousAddresses, address.Address.String())
 		}
 	}
 
@@ -182,10 +183,87 @@ func tcpSynFloodDetect(file string) Report {
 	return Report{suspicious: suspiciousAddresses, attacked: attackedAddresses}
 }
 
+// getSentSYNPackets gets all SYN packets sent by a given address using a slice of TCP packets
+func getSentSYNPackets(source gopacket.Endpoint, tcpPackets []TCPPacketAddress) []TCPPacketAddress {
+	// list to store sent tcp packets
+	var sentPackets []TCPPacketAddress
+
+	// loop through all tcp packets
+	for _, packet := range tcpPackets {
+		// if the packet source address matches the given address and is a SYN packet, add it to the return list
+		if packet.Source == source && packet.TcpPacket.SYN {
+			sentPackets = append(sentPackets, packet)
+		}
+	}
+
+	// sort the SYN packets by timestamp, ascending
+	sort.Slice(sentPackets, func(i, j int) bool {
+		return sentPackets[i].Packet.Metadata().Timestamp.Before(sentPackets[j].Packet.Metadata().Timestamp)
+	})
+
+	return sentPackets
+}
+
+// see if a string is in a slice of strings
+func contains(strings []string, word string) bool {
+	// loop through strings
+	for _, s := range strings {
+		// if the strings are equal, return true
+		if s == word {
+			return true
+		}
+	}
+	// no strings found
+	return false
+}
+
 // using the name of a pcap file and a given threshold, returns a slice containing any suspicious addresses
 func tcpConnectScanDetect(file string, threshold int) []string {
+	// constant time interval to determine how long a packet can send less SYN packets than the threshold
+	interval := 5 * time.Second
+	// get packets from a pcap file
+	packets := getPackets(file)
+	// extract all TCP layers
+	tcpPackets := getTCP(packets)
+	// collate address data
+	// collate address data
+	addresses := getTCPAddressData(tcpPackets)
+
 	// slices to contain suspicious and attacked addresses
 	var suspiciousAddresses []string
+
+	// *threaded* loop through addresses
+	for _, address := range addresses {
+		// proceed with determining if the address is suspicious if it has sent SYN flags without receiving SYN-ACK packets
+		if address.SendsSYN > 0 && address.ReceivesACK == 0 {
+			// get the TCP Packets sent by the address
+			sentSYNPackets := getSentSYNPackets(address.Address, tcpPackets)
+
+			// initialise lists to contain the tcp connection count and time
+			var tcpConnectionCount int
+			var tcpConnectionTime time.Time
+
+			// loop through sent packets
+			for _, packet := range sentSYNPackets {
+				// initialise the time
+				tcpConnectionTime = packet.Packet.Metadata().Timestamp
+				// If the number of SYN packet excels the threshold, check whether the time elapsed if less than interval.
+				// If so, the packet is classified as suspicious
+				if tcpConnectionCount >= threshold {
+					if packet.Packet.Metadata().Timestamp.Sub(tcpConnectionTime) <= interval {
+						// check if the address has been recorded already
+						if !contains(suspiciousAddresses, address.Address.String()) {
+							suspiciousAddresses = append(suspiciousAddresses, address.Address.String())
+						}
+					} else { // if not, reset
+						tcpConnectionCount = 0
+					}
+				}
+				// add one to the packet number count
+				tcpConnectionCount++
+			}
+		}
+	}
 
 	// return the suspicious and attacked addresses as a pair
 	return suspiciousAddresses
