1cfc49ce50946fc11818d0675e76134d7e9d3a65
AniBoja <aniboja26@gmail.com>
2024-03-15 23:20:35 +0000

    Rather than reading from the cap every time you make a filter change or stream change etc, the pcap is read in the constructor and mappings between stream ids and stream data is saved in a dictionary.

--- src/pythonGUI/follow_stream_window.py
+++ src/pythonGUI/follow_stream_window.py
@@ -10,25 +10,51 @@ class FollowStreamWindow(follow_stream.Ui_FollowStreamWindow, QMainWindow):
         self.mainWindow = mainWindow
         self.file_name = file_name
         self.setupUi(self)
+
+        stream_index = []
+        self.streams = {}
+        self.cap = pyshark.FileCapture(self.file_name)
+        for pkt in self.cap:
+            try:
+                stream_index.append(pkt.tcp.stream)
+            except:
+                pass
+        if len(stream_index) == 0:
+            max_index = 0
+        else:
+            max_index = int(max(stream_index)) + 1
+        components = 1
+        client = ""
+        server = ""
+        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+        for stream in range(0, max_index):
+            for pkt in self.cap:
+                try:
+                    if int(pkt.tcp.stream) == stream:
+                        if components % 2 == 1:
+                            client += ansi_escape.sub('',str(pkt.http).replace('\\n','').replace('\\r',''))
+                        if components % 2 == 0:
+                            server += ansi_escape.sub('',str(pkt.http).replace('\\n','').replace('\\r',''))
+                            self.streams[stream] = (client,server)
+                        components += 1
+                except:
+                    pass
+ 
+        self.streamNumberSpinBox.setMaximum(max_index - 1)
+
+        self.closePushButton.clicked.connect(self.close)
+        self.streamNumberSpinBox.valueChanged.connect(self.set_stream)
+        self.formatComboBox.currentTextChanged.connect(self.set_stream)
+
         self.formatComboBox.clear()
         self.formatComboBox.addItems(["ASCII", "Hex", "UTF-8", "UTF-16", "YAML"])
         self.set_stream()
-        
+    
     def set_stream(self):
         stream_index = self.streamNumberSpinBox.value()
-        cap = pyshark.FileCapture(self.file_name, display_filter=f"tcp.stream eq {stream_index}")
-        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-        conversation = ""
-        while True:
-            try:
-                pkt = cap.next()
-            except StopIteration:
-                break
-            try:
-                conversation += ansi_escape.sub('',str(pkt.http).replace('\\n','').replace('\\r',''))
-            except AttributeError:
-                pass
-        self.streamViewer.setText(conversation)
+        self.streamViewer.clear()
+        self.streamViewer.append(self.streams[stream_index][0])
+        self.streamViewer.append(self.streams[stream_index][1])
 
     def closeEvent(self, event):
         os.remove(self.file_name)
