b5811e7e5fee40157ac82997505befba2c6c31d9
AniBoja <aniboja26@gmail.com>
2024-03-16 11:38:05 +0000

    Categorise stream data as being from client-to-server or server-to-client by checking if pkt.http has a request attribute.

--- src/pythonGUI/follow_stream_window.py
+++ src/pythonGUI/follow_stream_window.py
@@ -12,7 +12,7 @@ class FollowStreamWindow(follow_stream.Ui_FollowStreamWindow, QMainWindow):
         self.setupUi(self)
 
         stream_index = []
-        self.streams = {}
+        self.streams = {"client_data": {}, "server_data": {}}
         self.cap = pyshark.FileCapture(self.file_name)
         for pkt in self.cap:
             try:
@@ -23,23 +23,22 @@ class FollowStreamWindow(follow_stream.Ui_FollowStreamWindow, QMainWindow):
             max_index = 0
         else:
             max_index = int(max(stream_index)) + 1
-        components = 1
-        client = ""
-        server = ""
         ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
         for stream in range(0, max_index):
+            client = ''
+            server = ''
             for pkt in self.cap:
                 try:
                     if int(pkt.tcp.stream) == stream:
-                        if components % 2 == 1:
-                            client += ansi_escape.sub('',str(pkt.http).replace('\\n','').replace('\\r',''))
-                        if components % 2 == 0:
-                            server += ansi_escape.sub('',str(pkt.http).replace('\\n','').replace('\\r',''))
-                            self.streams[stream] = (client,server)
-                        components += 1
+                            stream_data = ansi_escape.sub('',str(pkt.http).replace('\\n','').replace('\\r',''))
+                            if hasattr(pkt.http, "request"):
+                                client += stream_data
+                            else:
+                                server += stream_data
                 except:
                     pass
- 
+            self.streams["client_data"][stream] = client
+            self.streams["server_data"][stream] = server
         self.streamNumberSpinBox.setMaximum(max_index - 1)
 
         self.closePushButton.clicked.connect(self.close)
@@ -48,13 +47,17 @@ class FollowStreamWindow(follow_stream.Ui_FollowStreamWindow, QMainWindow):
 
         self.formatComboBox.clear()
         self.formatComboBox.addItems(["ASCII", "Hex", "UTF-8", "UTF-16", "YAML"])
+        self.conversationComboBox.clear()
+        self.conversationComboBox.addItems(["Entire conversation", "Client to server", "Server to client"])
+
         self.set_stream()
     
     def set_stream(self):
         stream_index = self.streamNumberSpinBox.value()
         self.streamViewer.clear()
-        self.streamViewer.append(self.streams[stream_index][0])
-        self.streamViewer.append(self.streams[stream_index][1])
+        self.streamViewer.append(self.streams["client_data"][stream_index])
+        self.streamViewer.append(self.streams["server_data"][stream_index])
+
 
     def closeEvent(self, event):
         os.remove(self.file_name)
